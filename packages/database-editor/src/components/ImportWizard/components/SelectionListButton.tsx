b8376e839ea58cef0093de66080bd7ba3722b6a9
Fabian Heuberger <fabian.heuberger@axonivy.com>
2025-11-11 15:17:05 +0100

    XIVY-17671 fixes icons in SelectionList

--- packages/database-editor/src/components/ImportWizard/components/SelectionListButton.tsx
+++ packages/database-editor/src/components/ImportWizard/components/SelectionListButton.tsx
@@ -2,19 +2,11 @@ import { Button, IvyIcon } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import './SelectionListButton.css';
 
-export const SelectionListButton = ({
-  value,
-  selected,
-  onClick
-}: {
-  value: string;
-  selected: boolean;
-  onClick: (table: string) => void;
-}) => {
+export const SelectionListButton = ({ value, onClick, icon }: { value: string; onClick: (table: string) => void; icon?: IvyIcons }) => {
   return (
     <Button className='selection-list-button' onClick={() => onClick(value)}>
       <span className='selection-list-button-text'>{value}</span>
-      <IvyIcon icon={selected ? IvyIcons.Close : IvyIcons.Plus} />
+      {icon && <IvyIcon icon={icon} />}
     </Button>
   );
 };
