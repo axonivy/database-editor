c629c875a8f83fd100450054d4b6b86c252f6573
Fabian Heuberger <fabian.heuberger@axonivy.com>
2026-01-07 12:03:29 +0100

    wip

--- packages/database-editor/src/components/editor/detail/DatabaseDetail.tsx
+++ packages/database-editor/src/components/editor/detail/DatabaseDetail.tsx
@@ -1,6 +1,7 @@
 import { Flex, SidebarHeader } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { useTranslation } from 'react-i18next';
+import { ConfigurationProperties } from './ConfigurationProperties';
 
 export const DatabaseDetail = () => {
   const { t } = useTranslation();
--- packages/database-editor/src/components/editor/master/DatabaseMasterContent.tsx
+++ packages/database-editor/src/components/editor/master/DatabaseMasterContent.tsx
@@ -1,12 +1,29 @@
 import type { DatabaseConnectionData } from '@axonivy/database-editor-protocol';
-import { BasicField, Button, Flex, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@axonivy/ui-components';
+import {
+  BasicField,
+  Button,
+  Flex,
+  SelectRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  useTableSelect
+} from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
+import { flexRender, getCoreRowModel, useReactTable, type ColumnDef } from '@tanstack/react-table';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppContext } from '../../../AppContext';
-import { SelectionListField } from '../../ImportWizard/components/SelectionList';
 import { ImportWizard } from '../../ImportWizard/ImportWizard';
 import { useDatabaseMutation } from '../useDatabaseMutation';
+import './DatabaseMasterContent.css';
 import { DbConnectionAddDialog } from './DbConnectionAddDialog';
 
 export const DatabaseMasterContent = ({
@@ -19,21 +36,68 @@ export const DatabaseMasterContent = ({
   const { setActiveDb } = useAppContext();
   const { databaseQuery } = useDatabaseMutation();
 
+  const columns: Array<ColumnDef<DatabaseConnectionData, string>> = [
+    {
+      accessorKey: 'name',
+      header: () => <span>{t('common.label.name')}</span>,
+      cell: cell => <span>{cell.getValue()}</span>
+    },
+    {
+      accessorKey: 'name',
+      header: () => <span>{t('database.host')}</span>,
+      cell: cell => <span>{cell.row.original.connectionProperties['Host'] as string}</span>
+    },
+    {
+      accessorKey: 'name',
+      header: () => <span>{t('database.port')}</span>,
+      cell: cell => <span>{cell.row.original.connectionProperties['Port'] as string}</span>
+    }
+  ];
+
+  const selection = useTableSelect<DatabaseConnectionData>();
+
+  const table = useReactTable({
+    ...selection.options,
+    data: databaseQuery.data ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      ...selection.tableState
+    }
+  });
+
   return (
-    <Flex direction='row' gap={4}>
+    <Flex direction='row' gap={4} className='database-master-content'>
       <BasicField style={{ width: '100%', height: '100%' }} label={t('database.allConnections')} control={<DbConnectionControls />}>
-        <SelectionListField
-          selectionTitle=''
-          items={databaseQuery.data?.map(d => d.name) ?? []}
-          onClick={value => {
-            const db = databaseQuery.data?.filter(d => d.name === value)[0];
-            if (db) {
-              setActiveDb(db);
-              setDetail(true);
-            }
-          }}
-          control={undefined}
-        />
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id} onClick={() => selection.options.onRowSelectionChange({})}>
+                {headerGroup.headers.map(header => (
+                  <TableHead key={header.id} colSpan={header.colSpan}>
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.map(row => (
+              <SelectRow
+                key={row.id}
+                row={row}
+                onClick={() => {
+                  setActiveDb(row.original);
+                  setDetail(true);
+                }}
+              >
+                {row.getVisibleCells().map(cell => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
+              </SelectRow>
+            ))}
+          </TableBody>
+        </Table>
       </BasicField>
     </Flex>
   );
