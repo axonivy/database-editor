d76e4da77b51c76366d1bf0fb8997cc54025624c
Lukas Lieb <lukas.lieb@axonivy.com>
2026-01-16 09:25:08 +0100

    Update react resizable panel

--- packages/database-editor/src/DatabaseEditor.tsx
+++ packages/database-editor/src/DatabaseEditor.tsx
@@ -1,5 +1,5 @@
 import type { DatabaseConfigurations, EditorProps } from '@axonivy/database-editor-protocol';
-import { Flex, PanelMessage, ResizableHandle, ResizablePanel, ResizablePanelGroup, Spinner } from '@axonivy/ui-components';
+import { Flex, PanelMessage, ResizableGroup, ResizableHandle, ResizablePanel, Spinner, useDefaultLayout } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useMemo, useState } from 'react';
@@ -13,7 +13,8 @@ import { useClient } from './protocol/ClientContextProvider';
 import { genQueryKey } from './query/query-client';
 
 export const DatabaseEditor = (props: EditorProps) => {
-  const [detail, setDetail] = useState(false);
+  const [detail, setDetail] = useState(true);
+  const { defaultLayout, onLayoutChanged } = useDefaultLayout({ groupId: 'database-editor-resize', storage: localStorage });
   const { t } = useTranslation();
   const context = useMemo(
     () => ({
@@ -63,20 +64,20 @@ export const DatabaseEditor = (props: EditorProps) => {
 
   return (
     <AppProvider projects={props.context.projects} context={context} data={data} setData={setData.mutate}>
-      <ResizablePanelGroup direction='horizontal'>
-        <ResizablePanel>
+      <ResizableGroup orientation='horizontal' defaultLayout={defaultLayout} onLayoutChanged={onLayoutChanged}>
+        <ResizablePanel id='main' defaultSize='50%' minSize='30%' className='database-editor-main-panel'>
           <DatabaseMasterToolbar detail={detail} setDetail={setDetail} />
           <DatabaseMasterContent setDetail={setDetail} />
         </ResizablePanel>
         {detail && (
           <>
             <ResizableHandle />
-            <ResizablePanel defaultSize={25} minSize={10}>
+            <ResizablePanel id='properties' defaultSize='25%' minSize='20%' className='database-editor-detail-panel'>
               <DatabaseDetail />
             </ResizablePanel>
           </>
         )}
-      </ResizablePanelGroup>
+      </ResizableGroup>
     </AppProvider>
   );
 };
